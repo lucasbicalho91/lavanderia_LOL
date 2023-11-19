cf9fbc829ae4be88b7dc0fe7dc0360b0bdb4f43b
LAPTOP-UOMTMQDU\Murilo Santos <Murilo dos Santos>
2023-11-19 14:33:05 -0300

    [Web2][Ajustes rodape e nav]

--- src/app/lavanderia/lavanderia.module.ts
+++ src/app/lavanderia/lavanderia.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CadastroComponent } from './cadastro/cadastro.component';
-import { LoginComponent } from './login/login.component';
 import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 import { RouterModule } from '@angular/router';
-import { RodapeComponent } from './rodape/rodape.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
@@ -13,9 +11,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     CadastroComponent,
-    LoginComponent,
-    PaginaInicialComponent,
-    RodapeComponent
+    PaginaInicialComponent
   ],
   imports: [
     CommonModule,
