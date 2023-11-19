fe5d285ee09c052f8293d387a2ab53adf5086ea1
LAPTOP-UOMTMQDU\Murilo Santos <Murilo dos Santos>
2023-11-19 16:54:02 -0300

    [Web2][Tela de Consulta Pedido Cliente]

--- src/app/cliente/consulta-pedido-cliente/consulta-pedido-cliente.component.ts
+++ src/app/cliente/consulta-pedido-cliente/consulta-pedido-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pedido, RoupasPedido } from 'src/app/shared';
 
 @Component({
   selector: 'app-consulta-pedido-cliente',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./consulta-pedido-cliente.component.css']
 })
 export class ConsultaPedidoClienteComponent {
+roupas: RoupasPedido[] = [
+{idPedido: 1, idRoupa: 34, nomeRoupa: 'Camiseta', valorLavagemPeca: 20.00},
+{idPedido: 1, idRoupa: 34, nomeRoupa: 'Camiseta', valorLavagemPeca: 20.00},
+{idPedido: 1, idRoupa: 36, nomeRoupa: 'Short', valorLavagemPeca: 10.00},
+{idPedido: 2, idRoupa: 34, nomeRoupa: 'Camiseta', valorLavagemPeca: 20.00},
+{idPedido: 2, idRoupa: 34, nomeRoupa: 'Calça', valorLavagemPeca: 30.00},
+{idPedido: 2, idRoupa: 34, nomeRoupa: 'Cueca', valorLavagemPeca: 12.00}
+];
+
+pedidos: Pedido[] = [
+  {idPedido: 1, dataPedido: '12/03/2023', dataEstimativa: '16/03/2023', dataColeta: '12/03/2023', dataEntrega: '15/03/2023', valor: 50.00, statusPedido: 'Finalizado'},
+  {idPedido: 2, dataPedido: '10/11/2023', dataEstimativa: '21/11/2023', dataColeta: '11/11/2023', dataEntrega: '', valor: 62.00, statusPedido: 'Em Andamento'}
+];
+
+roupa: RoupasPedido | undefined;
+pedido: number | null = null;
+detalhesPedido: Pedido = {};
+roupasPedido: RoupasPedido | undefined;
+
+buscarPedido(pedidoNumero?: number) {
+  const pedidoEncontrado = this.pedidos.filter(p => p.idPedido === pedidoNumero);
+  if (pedidoEncontrado.length > 0) {
+    this.detalhesPedido = pedidoEncontrado[0];
+  } else {
+    this.detalhesPedido = {};
+  }
+}
+
+listarPedidos() {
+  return this.roupas.filter(r => r.idPedido === this.pedido);
+}
 
 }
+
--- src/app/shared/models/pedido.model.ts
+++ src/app/shared/models/pedido.model.ts
@@ -1,10 +1,18 @@
 export class Pedido {
   constructor(
-    public idPedido: number,
-    public dataPedido: string,
-    public dataEstimativa: string,
-    public dataColeta: string,
-    public dataEntrega: string,
-    public valor: number,
-    public statusPedido: string) {}
+    public idPedido?: number,
+    public dataPedido?: string,
+    public dataEstimativa?: string,
+    public dataColeta?: string,
+    public dataEntrega?: string,
+    public valor?: number,
+    public statusPedido?: string) {}
+}
+
+export class RoupasPedido {
+  constructor(
+    public idPedido?: number,
+    public idRoupa?: number,
+    public nomeRoupa?: string,
+    public valorLavagemPeca?: number) {}
 }
