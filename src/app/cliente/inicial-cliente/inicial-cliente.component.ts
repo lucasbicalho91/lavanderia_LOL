ff0429cfe69b4197615cabcaf360b829bdac2c7a
Matheus Filipe <68246210+MatheusFilipeC@users.noreply.github.com>
2023-11-20 21:04:33 -0300

    Pagina inicial cliente ajustada.

--- src/app/cliente/inicial-cliente/inicial-cliente.component.ts
+++ src/app/cliente/inicial-cliente/inicial-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pedido } from 'src/app/shared';
 
 @Component({
   selector: 'app-inicial-cliente',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicial-cliente.component.css']
 })
 export class InicialClienteComponent {
+
+  pedido : Pedido | null = null;
+
+  pedidos : Pedido [] = [
+    {
+      idPedido : 123, dataPedido : new Date ('2023-05-01'), dataEstimativa : new Date ('2023-05-01'), dataColeta : new Date ('2023-05-01'), dataEntrega : new Date ('2023-05-01'), valor : 55.51, statusPedido : 'em andamento'
+  }
+];
 
 }
--- src/app/cliente/novo-pedido-cliente/novo-pedido-cliente.component.ts
+++ src/app/cliente/novo-pedido-cliente/novo-pedido-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pedido } from 'src/app/shared';
 
 @Component({
   selector: 'app-novo-pedido-cliente',
@@ -6,5 +7,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./novo-pedido-cliente.component.css']
 })
 export class NovoPedidoClienteComponent {
-
 }
