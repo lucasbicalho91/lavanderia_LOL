8264fd0f4d2c434c1a581cdb487705cfe07a62d2
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-11-27 19:03:10 -0300

    ajustes na página de novo pedido

--- src/app/cliente/novo-pedido-cliente/novo-pedido-cliente.component.ts
+++ src/app/cliente/novo-pedido-cliente/novo-pedido-cliente.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { PedidoService } from 'src/app/services/pedido.service';
 import { RoupaService } from 'src/app/services/roupa.service';
-import { Pedido, Roupa, RoupasPedido } from 'src/app/shared';
+import { Pedido, Roupa } from 'src/app/shared';
 
 @Component({
   selector: 'app-novo-pedido-cliente',
@@ -10,18 +9,42 @@ import { Pedido, Roupa, RoupasPedido } from 'src/app/shared';
   styleUrls: ['./novo-pedido-cliente.component.css']
 })
 export class NovoPedidoClienteComponent implements OnInit {
-  @ViewChild('formPedido') formPedido! : NgForm;
-  pedido!: Pedido;
+  pedido: Pedido = new Pedido();
   roupas: Roupa[] = [];
+  roupaSelecionada: Roupa | undefined;
 
   constructor(
     private pedidoService: PedidoService,
-    private roupaService: RoupaService) { }
+    private roupaService: RoupaService
+  ) {}
 
   ngOnInit(): void {
-    // Cris uma instância vazia, para não dar erro de referência
-    this.pedido = new Pedido();
-    this.pedido.roupas = [];
     this.roupas = this.roupaService.listarTodos();
+  }
+
+  adicionarItem(): void {
+    const roupasPedido = this.pedido?.roupas ?? [];
+    let valorPedido = this.pedido?.valor ?? 0;
+
+    if (this.roupaSelecionada && this.roupaSelecionada.precoRoupa !== undefined) {
+      roupasPedido.push({
+        nomeRoupa: this.roupaSelecionada.nomPecaRoupa,
+        valorLavagemPeca: this.roupaSelecionada.precoRoupa,
+      });
+
+      valorPedido += Number(this.roupaSelecionada.precoRoupa)
+      this.roupaSelecionada = undefined;
+      if (this.pedido) {
+        this.pedido.roupas = roupasPedido;
+        this.pedido.valor = valorPedido;
+      }
     }
+  }
+
+  onSubmit(): void {
+    // Adicionar lógica para enviar o pedido para o serviço ou onde for necessário
+    console.log('Pedido submetido:', this.pedido);
+    //this.pedidoService.enviarPedido(this.pedido);
+  }
+
 }
